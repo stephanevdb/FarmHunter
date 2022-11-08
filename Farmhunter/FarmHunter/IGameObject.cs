649ccb2f7d24012e5217b09b05b2558b710cf84b
stephanevdb <mail@stephanevdb.com>
2022-11-08 11:47:08 +0100

    add Interface

---
+++ Farmhunter/FarmHunter/IGameObject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmhunter.FarmHunter
+{
+    internal interface IGameObject
+    {
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
