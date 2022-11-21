509edc6bee23b498542361028140921f3a9606b0
Stéphane Van den Broeck <mail@stephanevdb.com>
2022-11-21 14:22:22 +0100

    add fractional control

--- Farmhunter/Control/KeyboardReader.cs
+++ Farmhunter/Control/KeyboardReader.cs
@@ -20,39 +20,53 @@ namespace Farmhunter.Control
                 GamePadState controllerState = GamePad.GetState(PlayerIndex.One);
                 if (controllerState.IsButtonDown(Buttons.DPadLeft) || controllerState.ThumbSticks.Left.X < 0)
                 {
-                    direction.X -= 1;
+                    if (controllerState.ThumbSticks.Left.X == 0)
+                    {
+                        direction.X -= 100;
+                    }
+                    else
+                    {
+                        direction.X -= controllerState.ThumbSticks.Left.X*100;
+                    }
                 }
                 if (controllerState.IsButtonDown(Buttons.DPadRight) || controllerState.ThumbSticks.Left.X > 0)
                 {
-                    direction.X += 1;
+                    if (controllerState.ThumbSticks.Left.X == 0)
+                    {
+                        direction.X += 100;
+                    }
+                    else
+                    {
+                        direction.X += controllerState.ThumbSticks.Left.X*100;
+                    }
                 }
 
                 if (controllerState.IsButtonDown(Buttons.RightShoulder))
                 {
-                    direction.Y -= 1;
+                    direction.Y -= 100;
                 }
                 if (controllerState.IsButtonDown(Buttons.A))
                 {
-                    direction.Y += 1;
+                    direction.Y += 100;
                 }
             }
             if (state.IsKeyDown(Keys.Left))
             {
-                direction.X -= 1;
+                direction.X -= 100;
             }
             if (state.IsKeyDown(Keys.Right))
             {
-                direction.X += 1;
+                direction.X += 100;
             }
 
             if (state.IsKeyDown(Keys.Space))
             {
-                
+                direction.Y += 100;
             }
 
             if (state.IsKeyDown(Keys.C))
             {
-                direction.Y -= 1;
+                direction.Y -= 100;
             }
             return direction;
         }
